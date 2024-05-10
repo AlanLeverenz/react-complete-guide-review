19fa2a6e4e6b2ade8dfdf66e33169c400759deaf
Alan Leverenz <awleverenz@aol.com>
2024-05-10 17:44:12 -0400

    added Header component file

---
+++ 01-invest-calc-project/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/investment-calculator.png'
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} />
+      <h1>Investment Calculator</h1>
+    </header>
+  )
+
+}
