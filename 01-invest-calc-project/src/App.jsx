92fde1444b4800211487d8ad426077e96e2a299b
Alan Leverenz <awleverenz@aol.com>
2024-05-13 15:38:02 -0400

    imported UserInput into App

--- 01-invest-calc-project/src/App.jsx
+++ 01-invest-calc-project/src/App.jsx
@@ -1,10 +1,12 @@
-import Header from './components/Header.jsx'
+import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <UserInput />
+    </>
   )
 }
 
