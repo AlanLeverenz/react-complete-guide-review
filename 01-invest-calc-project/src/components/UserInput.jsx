c0d037cde633cd42b0fb17f2ba3a88394ed678fd
Alan Leverenz <awleverenz@aol.com>
2024-05-13 15:07:43 -0400

    added UserInput component

---
+++ 01-invest-calc-project/src/components/UserInput.jsx
@@ -0,0 +1,14 @@
+export default function UserInput() {
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label></label>
+          <input />
+        </p>
+        <label></label>
+        <input />
+      </div>
+    </section>
+  )
+}
