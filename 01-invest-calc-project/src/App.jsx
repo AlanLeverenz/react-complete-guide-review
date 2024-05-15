42f2b39437655d856b07fd22cc66ec03ea1f82d9
Alan Leverenz <awleverenz@aol.com>
2024-05-15 12:11:29 -0400

    added Results component for calculating figures

--- 01-invest-calc-project/src/App.jsx
+++ 01-invest-calc-project/src/App.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
+
 import Header from './components/Header.jsx';
 import UserInput from './components/UserInput.jsx';
+// import Results from './components/Results.jsx;'
 
 function App() {
+
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
   return (
     <>
       <Header />
       <UserInput />
+      { /* RESULTS */}
     </>
   )
 }
