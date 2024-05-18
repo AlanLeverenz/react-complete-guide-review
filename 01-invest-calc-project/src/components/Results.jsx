81342329a377c492dc99af7095e039ed8e46cc12
Alan Leverenz <awleverenz@aol.com>
2024-05-18 08:18:24 -0400

    updated investment project

--- 01-invest-calc-project/src/components/Results.jsx
+++ 01-invest-calc-project/src/components/Results.jsx
@@ -23,15 +23,18 @@ export default function Results({ input }) {
       </thead>
       <tbody>
         {resultsData.map(yearData => {
-          const totalInterest = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year;
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year - initialinvestment;
+          const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
 
           return (
             <tr key={yearData.year}>
               <td>{yearData.year}</td>
               <td>{formatter.format(yearData.valueEndOfYear)}</td>
               <td>{formatter.format(yearData.interest)}</td>
-              <td>{formatter.format(yearData.interest)}</td>
-              <td>{(yearData.year)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
             </tr>
           )
         })}
