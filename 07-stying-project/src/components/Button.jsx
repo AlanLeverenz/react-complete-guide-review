87b2e07a798ca9ee83af897d120b29d083a7d79a
Alan Leverenz <awleverenz@aol.com>
2024-06-26 20:12:50 -0400

    updated Tailwind parameters in Button.jsx

--- 07-stying-project/src/components/Button.jsx
+++ 07-stying-project/src/components/Button.jsx
@@ -1,3 +1,3 @@
-export default function Button({ children }) {
-  return <button {...props}>{children}</button>
+export default function Button({ children, ...props }) {
+  return <button className="px-4 py-py-5 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500" {...props}>{children}</button>
 }
