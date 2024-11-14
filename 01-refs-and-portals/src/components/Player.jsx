5906365f34e07f7b7c5bf4ea30cde821fe00289d
Alan Leverenz <awleverenz@aol.com>
2024-11-14 14:34:29 -0500

    useState method

--- 01-refs-and-portals/src/components/Player.jsx
+++ 01-refs-and-portals/src/components/Player.jsx
@@ -1,9 +1,15 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+
+  function handleChange(event) { }
+
   return (
     <section id="player">
       <h2>Welcome unknown entity</h2>
       <p>
-        <input type="text" />
+        <input type="text" onChange={handleChange} />
         <button>Set Name</button>
       </p>
     </section>
