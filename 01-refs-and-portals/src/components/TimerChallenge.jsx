450542c3279d8de6a9031f7c290a4073492deb0b
Alan Leverenz <awleverenz@aol.com>
2024-12-13 14:39:22 -0500

    set handleStart function with timeout

--- 01-refs-and-portals/src/components/TimerChallenge.jsx
+++ 01-refs-and-portals/src/components/TimerChallenge.jsx
@@ -1,4 +1,14 @@
+import { useState } from 'react';
+
 export default function TimeChallenge({ title, targetTime }) {
+  const [timerExpired, setTimeExpired] = useState(false);
+
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+  }
+
   return <section className="challenge">
     <h2>{title}</h2>
     <p className="challenge-time">
