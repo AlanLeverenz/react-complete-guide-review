9905d795a0e54f166c7f72af16f746936a0f8126
Alan Leverenz <awleverenz@aol.com>
2024-12-19 15:52:08 -0500

    set modal to be conditional

--- 01-refs-and-portals/src/components/TimerChallenge.jsx
+++ 01-refs-and-portals/src/components/TimerChallenge.jsx
@@ -24,10 +24,10 @@ export default function TimeChallenge({ title, targetTime }) {
 
   return (
     <>
-      <ResultModal />
+      {timeExpired && <ResultModal targetTime={targetTime} result="lost" />}
       <section className="challenge">
         <h2>{title}</h2>
-        {timerExpired && <p>You lost!</p>}
+
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? 's' : ''}
         </p>
