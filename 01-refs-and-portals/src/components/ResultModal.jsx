dec9e22abc01d95fe7c636d3485d4cca4410063a
Alan Leverenz <awleverenz@aol.com>
2025-01-13 17:08:48 -0500

    updated ResultModal

--- 01-refs-and-portals/src/components/ResultModal.jsx
+++ 01-refs-and-portals/src/components/ResultModal.jsx
@@ -1,8 +1,11 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 
-export default function ResultModal({ ref, result, targetTime }) {
+export default function ResultModal({ ref, targetTime, remainingTime }) {
 
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   useImperativeHandle(ref, () => {
     return {
@@ -14,12 +17,12 @@ export default function ResultModal({ ref, result, targetTime }) {
 
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>Your {result}</h2>
+      {userLost && <h2>You lost</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
       <form method="dialog">
         <button>Close</button>
