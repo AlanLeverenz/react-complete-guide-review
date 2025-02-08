3b16d40e36e50eea2d45e82a77b08826884c8124
Alan Leverenz <awleverenz@aol.com>
2025-02-08 11:20:22 -0500

    added Tailwind styling

--- 01-project-management/src/components/ProjectsSidebar.jsx
+++ 01-project-management/src/components/ProjectsSidebar.jsx
@@ -1,7 +1,7 @@
 export default function ProjectsSidebar() {
 
   return (
-    <aside>
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2>Your projects</h2>
       <div>
         <button>
