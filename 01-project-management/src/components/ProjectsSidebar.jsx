7679a43d8f08882daaff9eae9069e6e80a51087c
Alan Leverenz <awleverenz@aol.com>
2025-02-08 11:26:48 -0500

    more styling added

--- 01-project-management/src/components/ProjectsSidebar.jsx
+++ 01-project-management/src/components/ProjectsSidebar.jsx
@@ -2,9 +2,9 @@ export default function ProjectsSidebar() {
 
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
-      <h2>Your projects</h2>
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your projects</h2>
       <div>
-        <button>
+        <button className="px-4 py-2 text-xs md:target:ext-base rounded-mdbg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
           + Add Project
         </button>
       </div>
