a1b05ef666217e8bf3125b9fbc93b9a3390d5869
Alan Leverenz <awleverenz@aol.com>
2025-02-12 22:31:22 -0500

    set state for added projects

--- 01-project-management/src/App.jsx
+++ 01-project-management/src/App.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
+
 import ProjectsSidebar from "./components/ProjectsSidebar";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from './components/NoProjectSelected';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectedId: null,
+      }
+    });
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
