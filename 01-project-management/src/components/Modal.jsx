1ecb745ed16ad0ac7dccdef4b24d0f3db9c1ad07
Alan Leverenz <awleverenz@aol.com>
2025-02-16 17:20:46 -0500

    added Modal component

--- 01-project-management/src/components/Modal.jsx
+++ 01-project-management/src/components/Modal.jsx
@@ -1,11 +1,22 @@
-import { forwardRef, useImperativeHandle } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 // forwardRef not needed for React 19 and newer
 
-const Modal = forwardRef(function Modal({ children }) {
+const Modal = forwardRef(function Modal({ children }, ref) {
+  const dialog = useRef();
+  // to call this function from elsewhere
+  // connect ref to dialog element
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      }
+    }
+  });
+
   return createPortal(
-    <dialog>
+    <dialog ref={dialog}>
       {children}
     </dialog>, document.getElementById('modal-root')
   );
