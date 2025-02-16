2597e4f9b46f349d1efa950ea50e82cc61d59581
Alan Leverenz <awleverenz@aol.com>
2025-02-16 17:08:06 -0500

    applying forwardRef

--- 01-project-management/src/components/Modal.jsx
+++ 01-project-management/src/components/Modal.jsx
@@ -1,9 +1,14 @@
+import { forwardRef, useImperativeHandle } from 'react';
 import { createPortal } from 'react-dom';
 
-export default function Modal({ children }) {
+// forwardRef not needed for React 19 and newer
+
+const Modal = forwardRef(function Modal({ children }) {
   return createPortal(
     <dialog>
       {children}
     </dialog>, document.getElementById('modal-root')
   );
-}
+});
+
+export default Modal;
