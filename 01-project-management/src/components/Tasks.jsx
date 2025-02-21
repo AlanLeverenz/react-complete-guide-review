524b6eb7b24b9bb1c939fd97a10e36d8ceaafd29
Alan Leverenz <awleverenz@aol.com>
2025-02-21 18:38:48 -0500

    added NewTask component

--- 01-project-management/src/components/Tasks.jsx
+++ 01-project-management/src/components/Tasks.jsx
@@ -2,8 +2,8 @@ export default function Tasks() {
   return (
     <section>
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
-      NEW TASKS
-      <p>This project does not have any tasks yet.</p>
+      NEW TASK
+      <p className="text-stone-800 mb-4">This project does not have any tasks yet.</p>
       <ul></ul>
     </section>
   )
