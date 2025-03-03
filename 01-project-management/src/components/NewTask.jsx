abc831411f1a5a0180815ae5de57f8b16b377339
Alan Leverenz <awleverenz@aol.com>
2025-03-03 15:27:45 -0500

    two-way binding with input field and state

--- 01-project-management/src/components/NewTask.jsx
+++ 01-project-management/src/components/NewTask.jsx
@@ -4,7 +4,7 @@ export default function NewTask() {
   const [enteredTask, setEnteredTask] = useState();
 
   function handleChange(event) {
-
+    setEnteredTask(event.target.value);
   }
 
   return (
@@ -13,6 +13,7 @@ export default function NewTask() {
         type="text"
         className="w-64 px-2 py-1 rounded-sm bg-stone-200"
         onChange={handleChange}
+        value={enteredTask}
       />
       <button className="text-stone-700 hover:text-stone-950">Add Task</button>
     </div>
