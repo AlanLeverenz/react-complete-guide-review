435bd48416f31df5a89949b3e466a211f8cc95d4
Alan Leverenz <awleverenz@aol.com>
2025-03-03 16:27:10 -0500

    styled Tasks list

--- 01-project-management/src/components/Tasks.jsx
+++ 01-project-management/src/components/Tasks.jsx
@@ -9,13 +9,16 @@ export default function Tasks({ tasks, onAdd, onDelete }) {
         <p className="text-stone-800 my-4">This project does not have any tasks yet.
         </p>
       )}
-      {tasks.length > 0 && <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <span>{task.text}</span>
-            <button>Clear</button>
-          </li>))}
-      </ul>}
+      {tasks.length > 0 &&
+        <ul className="p-4 mt-8 rounded-md bg-stone-100">
+          {tasks.map((task) => (
+            <li key={task.id} className="flex justify-between my-4">
+              <span>{task.text}</span>
+              <button className="text-stone-700 hover: trext-red-500">
+                Clear
+              </button>
+            </li>))}
+        </ul>}
     </section>
   );
 }
