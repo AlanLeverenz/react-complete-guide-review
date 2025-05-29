7b1ecc501a24e2cdaaa9c364084a1463abfd4455
Alan Leverenz <awleverenz@aol.com>
2025-05-29 18:31:38 -0400

    remove all props and used context within each component directly

--- 01-cart-demo-project/src/App.jsx
+++ 01-cart-demo-project/src/App.jsx
@@ -70,17 +70,15 @@ function App() {
 
   const ctxValue = {
     items: shoppingCart.items,
-    addItemToCart: handleAddItemToCart
+    addItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
   };
 
   // console.log(ctxValue);
 
   return (
     <CartContext.Provider value={ctxValue}>
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
+      <Header />
       <Shop>
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
