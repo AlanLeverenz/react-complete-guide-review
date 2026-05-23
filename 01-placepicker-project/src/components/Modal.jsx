ebd9308e690441983d4182f1b7a1daf21438594c
Alan Leverenz <awleverenz@aol.com>
2026-05-22 21:49:34 -0400

    applied useEffect for modal dialog control

--- 01-placepicker-project/src/components/Modal.jsx
+++ 01-placepicker-project/src/components/Modal.jsx
@@ -1,8 +1,18 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 function Modal({ open, children }) {
   const dialog = useRef();
+
+  // useEffect handles the DOM API
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, []);
+  // any value that causes the code to execute again is a dependency (state, prop)
 
   return createPortal(
     <dialog className="modal" ref={dialog} open={open}>
