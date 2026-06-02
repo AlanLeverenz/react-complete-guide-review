dde4c2eeed735bdcc0e77234bebea234fceb937a
Alan Leverenz <awleverenz@aol.com>
2026-06-01 22:55:50 -0400

    updated Quiz component

--- 01-react quiz/src/components/Quiz.jsx
+++ 01-react quiz/src/components/Quiz.jsx
@@ -1,8 +1,19 @@
 import { useState } from 'react';
 
+import QUESTIONS from '../questions.js';
+
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently active Question</p>;
+  const activeQuestionIndex = userAnswers.length;
+
+  return (
+    <div id="question">
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        { }
+      </ul>
+
+    </div>
+  );
 }
