0859fb13438e18745ffbef4b88c059991016ccd1
Alan Leverenz <awleverenz@aol.com>
2026-06-04 23:32:58 -0400

    added useEffect for setTimeout and setInterval

--- 01-react quiz/src/components/QuestionTime.jsx
+++ 01-react quiz/src/components/QuestionTime.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
-  setTimeout(onTimeout, timeout);
+  // to set a time limit for answering a question in the quiz
+  // apply useEffect to avoid creating multiple timers
+  // will run only if/when the dependencies change
+  useEffect(() => {
+    setTimeout(onTimeout, timeout);
+  }, [timeout, onTimeout])
 
-  setInterval(() => {
-    setRemainingTime();
-  }, 100);
+
+  useEffect(() => {
+    // for setting the increments of the progress bar
+    // useEffect avoids an infinite loop updating remainingTime
+    // it runs only once if there are no dependencies
+    setInterval(() => {
+      setRemainingTime(prevRemainingTime => prevRemainingtime - 100);
+    }, 100);
+  }, []);
+
+
 
   return <progress id="question-time" />;
 }
