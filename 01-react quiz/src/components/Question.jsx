62c40d8e00e20046107e78cf4c2e0c5761ccf4dc
Alan Leverenz <awleverenz@aol.com>
2026-06-16 11:36:39 -0400

    set condition for when to skip to the next question

--- 01-react quiz/src/components/Question.jsx
+++ 01-react quiz/src/components/Question.jsx
@@ -56,11 +56,14 @@ export default function Question({
     answerState = 'answered'
   }
 
+  // use key to unmount and mount the component's timer interval)
+  // only skip question if an answer was not selected
   return (
     <div id="question">
       <QuestionTimer
+        key={timer}
         timeout={timer}
-        onTimeout={onSkipAnswer}
+        onTimeout={answer.selectedAnswer === '' ? onSkipAnswer : null}
         mode={answerState}
       />
       <h2>{QUESTIONS[index].text}</h2>
